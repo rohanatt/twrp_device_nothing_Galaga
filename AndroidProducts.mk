1195c272f39ba446b5615dbf0b3130005b3e1b82
rohanatt <rohanatts12@gmail.com>
2026-04-05 13:08:39 +0600

    Fix AndroidProducts.mk lunch target

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,2 @@
-# Replace $$DEVICE$$ with your Device Name's Value.
-# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
-# The part of last line in mine looks like "omni_Primo_RX5.mk"
-
-LOCAL_PATH := device/$$BRAND$$/$$DEVICE$$
-
-PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_Galaga.mk
+PRODUCT_MAKEFILES := $(LOCAL_PATH)/twrp_Galaga.mk
+COMMON_LUNCH_CHOICES := twrp_Galaga-eng
